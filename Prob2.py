ecc401873de788856620bc8129170376aba133cb
noahkla <noahklarreich@gmail.com>
2024-10-14 11:13:55 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -12,16 +12,28 @@ BRICK_WIDTH = 40
 BRICK_HEIGHT = 20
 BRICKS_IN_BASE = 15
 
+    
 def draw_pyramid():
     """ 
     Draws a pyramid of bricks centered on the screen with a height of BRICKS_IN_BASE. 
     """
 
     gw = GWindow(WIDTH, HEIGHT)
+    h = HEIGHT-100
+    for i in range(BRICKS_IN_BASE):
+        row(BRICKS_IN_BASE - i, h, gw)
+        h -= BRICK_HEIGHT
 
-    # You got it from here
 
-
+    
+def row(n, h, gw):
+    length = n*BRICK_WIDTH
+    s  = WIDTH/2 - length/2
+    for i in range(n):
+        r = GRect(s, h, BRICK_WIDTH, BRICK_HEIGHT)
+        r.set_color('Black')
+        gw.add(r)
+        s += BRICK_WIDTH
 
 
 
